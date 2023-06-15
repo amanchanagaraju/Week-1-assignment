68f1b5ee95cd7a75554cac56688f1fe010787691
Nagaraju Amancha <132131971+amanchanagaraju@users.noreply.github.com>
2023-06-15 16:06:41 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -1,18 +1,33 @@
-/*
-  Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
 
-  Once you've implemented the logic, test your code by running
-  - `npm run test-todo-list`
-*/
+ class Todo {
 
-class Todo {
-
+  todos = [];
+  add(str) {
+    this.todos.push(str);
+  }
+  remove(index) {
+    if (index < this.todos.length) {
+      this.todos.splice(index, 1);
+    }
+  }
+  update(index, updatedTodo) {
+    if (index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    }
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    } else {
+      return null;
+    }
+  }
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
