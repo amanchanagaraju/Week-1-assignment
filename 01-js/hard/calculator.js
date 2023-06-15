3975ffcbafeeda94af02c0629ec850e67ed79f6b
Nagaraju Amancha <132131971+amanchanagaraju@users.noreply.github.com>
2023-06-15 16:03:48 +0530

    Update calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,22 +1,74 @@
-/*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
-    - add: takes a number and adds it to the result
-    - subtract: takes a number and subtracts it from the result
-    - multiply: takes a number and multiply it to the result
-    - divide: takes a number and divide it to the result
-    - clear: makes the `result` variable to 0
-    - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
-        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
-  Once you've implemented the logic, test your code by running
-  - `npm run test-calculator`
-*/
+class Calculator {
+  constructor() {
+    this.result = 0
+  }
 
-class Calculator {}
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if(number === 0) {
+      throw new Error("Division by zero is not possible");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+
+    const cleanExpression = expression.replace(/\s+/g, "")
+
+    if (!/^[0-9+\-*/().\s]+$/.test(cleanExpression)) {
+      throw new Error("Invalid input. Expression contains non-numeric characters.");
+    }
+
+    try {
+      this.result = eval(cleanExpression);
+      if (isNaN(this.result)) {
+        throw new Error("Invalid input. Unable to evaluate the expression.");
+      }
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        throw new Error("Invalid input. Unable to evaluate the expression.");
+      } else {
+        throw error;
+      }
+    }    
+  }
+}
+
+const calculator = new Calculator();
+
+calculator.add(5);
+calculator.subtract(3);
+console.log(calculator.getResult()); // Output: 2
+
+calculator.multiply(4);
+calculator.divide(2);
+console.log(calculator.getResult()); // Output: 4
+
+calculator.clear();
+console.log(calculator.getResult()); // Output: 0
+
+calculator.calculate("10 + 2 * (6 - (4 + 1) / 2) + 7");
+console.log(calculator.getResult()); // Output: 24
+
 
 module.exports = Calculator;
